e878874756c52e9025979091781da3e2da747aa9
Ravi Pandya <rpandya922@berkeley.edu>
2017-05-31 16:22:19 -0700

    added data collection with kinematic model

---
+++ src/arm_joints_recorder.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+from __future__ import division  
+import rospy
+import math
+import tf
+import geometry_msgs.msg
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import scipy
+import scipy.io
+from sklearn.svm import SVC
+from random import shuffle
+import sys
+import select
+import copy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import baxter_interface.gripper as gripper
+import sklearn.neighbors as neighbors
+import sensor_msgs.msg as sensor_msgs
+
+def heardEnter():
+    i,o,e = select.select([sys.stdin],[],[],0.0001)
+    for s in i:
+        if s == sys.stdin:
+            input = sys.stdin.readline()
+            return True
+    return False
+
+def move():
+    pose = geometry_msgs.msg.Pose()
+    x = np.random.uniform(0.5, 1.1, 1)[0]
+    y = 0.28
+    z = np.random.uniform(0.3, 0.8, 1)[0]
+    pose.position.x = x
+    pose.position.y = y
+    pose.position.z = z
+    pose.orientation.x = 0.5179492136281051
+    pose.orientation.y = -0.4655689719259183
+    pose.orientation.z = 0.5199804480287061
+    pose.orientation.w = -0.4945649382535493
+
+    group.set_pose_target(pose)
+    plan = group.plan()
+    group.go(wait=True)
+
+def preprocess(data):
+    X = np.array([np.array(s[0]) for s in data])
+    y = np.array([s[1][3:] for s in data])
+    return X, y
+def predict(pred, real):
+    for i in range(len(pred)):
+        p = pred[i]
+        y = real[i]
+        print min([np.linalg.norm(p - y), np.linalg.norm(-p - y)])
+
+print "============ Starting tutorial setup"
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('erg',
+                anonymous=True)
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
+# group = moveit_commander.MoveGroupCommander("right_arm")
+group = moveit_commander.MoveGroupCommander("left_arm")
+display_trajectory_publisher = rospy.Publisher(
+                                    '/move_group/display_planned_path',
+                                    moveit_msgs.msg.DisplayTrajectory)
+print "============ Reference frame: %s" % group.get_planning_frame()
+print "============ Reference frame: %s" % group.get_end_effector_link()
+group.set_goal_tolerance(0.05)
+
+global sb
+global pos
+pos = []
+def callback(data):
+    global pos
+    pos = data.position
+    sb.unregister()
+# x = [0.6, 1]
+# y = 0.28
+# z = [0.1, 0.8]
+all_data = []
+if __name__ == '__main__':
+    rospy.on_shutdown(lambda: np.save("arm_joints_data", all_data))
+    listener = tf.TransformListener()
+    while not rospy.is_shutdown():
+        if heardEnter():
+            move()
+            while not heardEnter():
+                continue
+            sb = rospy.Subscriber("kinmodel_state", sensor_msgs.JointState, callback)
+            rospy.sleep(1)
+            print "Recorded Position: " + str(pos)
+            (trans, rot) = listener.lookupTransform('/base', 'left_gripper', rospy.Time(0))
+            all_data.append((trans, pos))
