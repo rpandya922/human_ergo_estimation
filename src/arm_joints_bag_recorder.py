c7d287072692a41f56c5970170393b3ace678f72
Ravi Pandya <rpandya922@berkeley.edu>
2017-06-02 13:27:23 -0700

    separated recorder for live and bagfile data

--- src/arm_joints_bag_recorder.py
+++ src/arm_joints_bag_recorder.py
@@ -29,47 +29,14 @@ def heardEnter():
             return True
     return False
 
-def move():
-    pose = geometry_msgs.msg.Pose()
-    x = np.random.uniform(0.5, 1.1, 1)[0]
-    y = 0.28
-    z = np.random.uniform(0.3, 0.8, 1)[0]
-    pose.position.x = x
-    pose.position.y = y
-    pose.position.z = z
-    pose.orientation.x = 0.5179492136281051
-    pose.orientation.y = -0.4655689719259183
-    pose.orientation.z = 0.5199804480287061
-    pose.orientation.w = -0.4945649382535493
-
-    group.set_pose_target(pose)
-    plan = group.plan()
-    group.go(wait=True)
-
-print "============ Starting tutorial setup"
-moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node('erg',
-                anonymous=True)
-robot = moveit_commander.RobotCommander()
-scene = moveit_commander.PlanningSceneInterface()
-# group = moveit_commander.MoveGroupCommander("right_arm")
-group = moveit_commander.MoveGroupCommander("left_arm")
-display_trajectory_publisher = rospy.Publisher(
-                                    '/move_group/display_planned_path',
-                                    moveit_msgs.msg.DisplayTrajectory)
-print "============ Reference frame: %s" % group.get_planning_frame()
-print "============ Reference frame: %s" % group.get_end_effector_link()
-group.set_goal_tolerance(0.05)
-
+rospy.init_node('recorder', anonymous=True)
 global sb
 global pos
 pos = []
 def callback(data):
     global pos
     pos = data.position
-# x = [0.6, 1]
-# y = 0.28
-# z = [0.1, 0.8]
+
 all_data = []
 if __name__ == '__main__':
     rospy.on_shutdown(lambda: np.save("arm_joints_bag_data", all_data))
@@ -78,9 +45,6 @@ if __name__ == '__main__':
     sb = rospy.Subscriber("kinmodel_state", sensor_msgs.JointState, callback)
     while not rospy.is_shutdown():
         if heardEnter():
-            move()
-            while not heardEnter():
-                continue
             print "Recorded Position: " + str(pos)
             (trans, rot) = listener.lookupTransform('/base', 'left_gripper', rospy.Time(0))
             print "Baxter Arm: " + str(trans)
