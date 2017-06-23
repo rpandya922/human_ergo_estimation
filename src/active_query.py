4a31c3ccb31871f7c8e9954e6a1020397f801a97
Ravi Pandya <rpandya922@berkeley.edu>
2017-06-23 13:13:06 -0700

    parallelized computation of info gain

--- src/active_query.py
+++ src/active_query.py
@@ -8,6 +8,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import probability_estimation as pe
 from distribution import ParticleDistribution
 from random import shuffle
+import multiprocessing as mp
 
 #########################################################
 # CONSTANTS AND FUNCTIONS
@@ -22,13 +23,9 @@ def cost(theta, theta_star, w):
     return pe.distance_cost(theta, theta_star, w)
 def prior(vec):
     return 1
-def info_gain(dist, feasible):
-    return dist.info_gain(feasible, num_boxes, axis_ranges)
-#########################################################
 data = np.load('./sim_training_data.npy')
 shuffle(data)
 data = data[:12]
-
 particles = []
 weights = []
 for i in range(NUM_PARTICLES):
@@ -43,27 +40,30 @@ weights = np.array(weights) / np.sum(weights)
 dist = ParticleDistribution(particles, weights, cost)
 dist2 = ParticleDistribution(particles, weights, cost)
 (theta1, feas1) = data[0]
-
-same_entropy = []
-max_info_entropy = []
-same_entropy.append(dist.entropy(num_boxes, axis_ranges))
-max_info_entropy.append(dist2.entropy(num_boxes, axis_ranges))
-for i in range(6):
-    dist.weights = dist.reweight(theta1, feas1)
-    dist.resample()
+def info_gain(x):
+    return (x, dist2.info_gain(x[1], num_boxes, axis_ranges))
+#########################################################
+if __name__ == '__main__':
+    pool = mp.Pool(5)
+    same_entropy = []
+    max_info_entropy = []
     same_entropy.append(dist.entropy(num_boxes, axis_ranges))
-
-    (theta_max, feas_max) = min(data, key=lambda x: info_gain(dist2, x[1]))
-    dist2.weights = dist2.reweight(theta_max, feas_max)
-    dist2.resample()
     max_info_entropy.append(dist2.entropy(num_boxes, axis_ranges))
-    print i
+    for i in range(5):
+        dist.weights = dist.reweight(theta1, feas1)
+        dist.resample()
+        same_entropy.append(dist.entropy(num_boxes, axis_ranges))
+        (theta_max, feas_max) = min(pool.map(info_gain, data), key=lambda x: x[1])[0]
+        dist2.weights = dist2.reweight(theta_max, feas_max)
+        dist2.resample()
+        max_info_entropy.append(dist2.entropy(num_boxes, axis_ranges))
+        print i
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_xlabel('iteration')
-ax.set_ylabel('entropy')
-ax.plot(range(len(same_entropy)), same_entropy, label='same sample')
-ax.plot(range(len(max_info_entropy)), max_info_entropy, label='max info sample')
-plt.legend(loc='upper left')
-plt.show()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('iteration')
+    ax.set_ylabel('entropy')
+    ax.plot(range(len(same_entropy)), same_entropy, label='same sample')
+    ax.plot(range(len(max_info_entropy)), max_info_entropy, label='max info sample')
+    plt.legend(loc='upper left')
+    plt.show()
