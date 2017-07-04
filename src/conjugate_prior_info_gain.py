0593c2c045a7a8df5f4a36f0a95366ac63ff89eb
Ravi Pandya <rpandya922@berkeley.edu>
2017-07-03 17:24:30 -0700

    visualizations of entropy change

--- src/conjugate_prior_info_gain.py
+++ src/conjugate_prior_info_gain.py
@@ -213,50 +213,218 @@ def blend(color, alpha, base=[255,255,255]):
     return [int(round((alpha * color[i]) + ((1 - alpha) * base[i]))) for i in range(3)]
 def to_hex(color):
     return '#' + ''.join(["%02x" % e for e in color])
+def plot_info_gain_pca(data):
+    means = np.zeros(DOF)
+    taus = np.ones(DOF) * 0.001
+    alphas = np.ones(DOF) * 2
+    betas = np.ones(DOF) / 2
+
+    means, taus, alphas, betas = update(means, taus, alphas, betas, [data[0][1][0]])
+
+    fig, axes = plt.subplots(nrows=2, ncols=4, sharex=True, sharey=True)
+    axes = np.ndarray.flatten(np.array(axes))
+
+    full_feasible = []
+    full_feasible.append(data[0][1][0])
+    for (thetas, feasible) in data:
+        idxs = np.random.choice(len(feasible), size=100)
+        full_feasible.extend(feasible[idxs])
+    full_feasible = np.array(full_feasible)
+    print len(full_feasible)
+    infos = []
+    for theta in full_feasible:
+        infos.append(info_gain(means, taus, alphas, betas, [theta]))
+    print np.amin(infos)
+    print np.amax(infos)
+    print
+    infos = (np.array(infos) - np.amin(infos))/ np.amax(infos)
+    colors = [to_hex(blend([120, 51, 255], inf)) for inf in infos]
+
+    model = PCA(n_components=2)
+    new_data = model.fit_transform(full_feasible)
+    point = new_data[0]
+    ax = axes[0]
+    ax.scatter(point[0], point[1], c='r', zorder=1)
+    ax.scatter(new_data[:,0], new_data[:,1], lw=0, s=5, c=colors, zorder=0)
+
+    points = [point]
+    for i in range(1,8):
+        idx = np.argmax(infos)
+        points.append(new_data[idx])
+        means, taus, alphas, betas = update(means, taus, alphas, betas, [full_feasible[idx]])
+
+        infos = []
+        for theta in full_feasible:
+            infos.append(info_gain(means, taus, alphas, betas, [theta]))
+        print np.amin(infos)
+        print np.amax(infos)
+        print
+        infos = (np.array(infos) - np.amin(infos))/ np.amax(infos)
+        colors = [to_hex(blend([120, 51, 255], inf)) for inf in infos]
+
+        ax = axes[i]
+        for point in points:
+            ax.scatter(point[0], point[1], c='r', zorder=1)
+        ax.scatter(new_data[:,0], new_data[:,1], lw=0, s=5, c=colors, zorder=0)
+    plt.show()
+def plot_min_cost_pca(data):
+    means = np.zeros(DOF)
+    taus = np.ones(DOF) * 0.001
+    alphas = np.ones(DOF) * 2
+    betas = np.ones(DOF) / 2
+
+    means, taus, alphas, betas = update(means, taus, alphas, betas, [data[0][1][0]])
+    lam = np.hstack(((alphas - 1) / betas, means))
+
+    fig, axes = plt.subplots(nrows=2, ncols=4, sharex=True, sharey=True)
+    axes = np.ndarray.flatten(np.array(axes))
+
+    full_feasible = []
+    full_feasible.append(data[0][1][0])
+    for (thetas, feasible) in data:
+        idxs = np.random.choice(len(feasible), size=100)
+        full_feasible.extend(feasible[idxs])
+    full_feasible = np.array(full_feasible)
+
+    infos = []
+    costs = []
+    for theta in full_feasible:
+        infos.append(info_gain(means, taus, alphas, betas, [theta]))
+        costs.append(cost(theta, lam[DOF:], lam[:DOF]))
+    infos = (np.array(infos) - np.amin(infos))/ np.amax(infos)
+    colors = [to_hex(blend([120, 51, 255], inf)) for inf in infos]
+
+    model = PCA(n_components=2)
+    new_data = model.fit_transform(full_feasible)
+    point = new_data[0]
+    ax = axes[0]
+    ax.scatter(point[0], point[1], c='r', zorder=1)
+    ax.scatter(new_data[:,0], new_data[:,1], lw=0, s=5, c=colors, zorder=0)
+
+    points = [point]
+    for i in range(1,8):
+        idx = np.argmin(costs)
+        points.append(new_data[idx])
+        prev = full_feasible[idx]
+        means, taus, alphas, betas = update(means, taus, alphas, betas, [full_feasible[idx]])
+
+        infos = []
+        costs = []
+        for theta in full_feasible:
+            infos.append(info_gain(means, taus, alphas, betas, [theta]))
+            costs.append(cost(theta, lam[DOF:], lam[:DOF]))
+
+        infos = (np.array(infos) - np.amin(infos))/ np.amax(infos)
+        colors = [to_hex(blend([120, 51, 255], inf)) for inf in infos]
+
+        ax = axes[i]
+        for point in points:
+            ax.scatter(point[0], point[1], c='r', zorder=1)
+        ax.scatter(new_data[:,0], new_data[:,1], lw=0, s=5, c=colors, zorder=0)
+    plt.show()
+def expected_info_gain(feasible, means, taus, alphas, betas, lam, ALPHA):
+    nums = np.zeros(len(feasible))
+    infos = np.zeros(len(feasible))
+    for i in range(len(feasible)):
+        theta = feasible[i]
+        nums[i] = pe.prob_stable2_num(theta, lam, cost, ALPHA)
+        infos[i] = info_gain(means, taus, alphas, betas, [theta])
+    denom = pe.prob_stable2_denom(feasible, lam, cost, ALPHA)
+    probs = np.exp(nums - denom)
+    return np.sum(infos * probs)
+def expected_cost(feasible, lam, ALPHA):
+    nums = np.zeros(len(feasible))
+    costs = np.zeros(len(feasible))
+    for i in range(len(feasible)):
+        theta = feasible[i]
+        nums[i] = pe.prob_stable2_num(theta, lam, cost, ALPHA)
+        costs[i] = cost(theta, lam[DOF:], lam[:DOF])
+    denom = pe.prob_stable2_denom(feasible, lam, cost, ALPHA)
+    probs = np.exp(nums - denom)
+    return np.sum(costs * probs)
+def argmax_info_gain(feasible, means, taus, alphas, betas):
+    return np.amax([info_gain(means, taus, alphas, betas, [theta]) for theta in feasible])
+def argmin_cost(feasible, lam):
+    return np.amin([cost(theta, lam[DOF:], lam[:DOF]) for theta in feasible])
 #######################################################
 data = np.load("./full_sim_k_training_data_0.npy")
-# shuffle(data)
+
+sampled_data = []
+for (thetas, feasible) in data:
+    idxs = np.random.choice(len(feasible), size=1000)
+    sampled_data.append((thetas, feasible[idxs]))
+data = sampled_data
 
 means = np.zeros(DOF)
 taus = np.ones(DOF) * 0.001
 alphas = np.ones(DOF) * 2
 betas = np.ones(DOF) / 2
 
-means, taus, alphas, betas = update(means, taus, alphas, betas, [data[0][1][0]])
+means2 = np.zeros(DOF)
+taus2 = np.ones(DOF) * 0.001
+alphas2 = np.ones(DOF) * 2
+betas2 = np.ones(DOF) / 2
 
-full_feasible = []
-full_feasible.append(data[0][1][0])
-for (thetas, feasible) in data:
-    idxs = np.random.choice(len(feasible), size=100)
-    full_feasible.extend(feasible[idxs])
-full_feasible = np.array(full_feasible)
-print len(full_feasible)
-infos = []
-for theta in full_feasible:
-    infos.append(info_gain(means, taus, alphas, betas, [theta]))
-print np.amin(infos)
-print np.amax(infos)
-infos = (np.array(infos) - np.amin(infos))/ np.amax(infos)
-colors = [to_hex(blend([120, 51, 255], i)) for i in infos]
+means3 = np.zeros(DOF)
+taus3 = np.ones(DOF) * 0.001
+alphas3 = np.ones(DOF) * 2
+betas3 = np.ones(DOF) / 2
 
-model = PCA(n_components=2)
-new_data = model.fit_transform(full_feasible)
-point = new_data[0]
+means4 = np.zeros(DOF)
+taus4 = np.ones(DOF) * 0.001
+alphas4 = np.ones(DOF) * 2
+betas4 = np.ones(DOF) / 2
+
+means5 = np.zeros(DOF)
+taus5 = np.ones(DOF) * 0.001
+alphas5 = np.ones(DOF) * 2
+betas5 = np.ones(DOF) / 2
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(point[0], point[1], s=200, c='r', zorder=1)
-ax.scatter(new_data[:,0], new_data[:,1], lw=0, s=5, c=colors, zorder=0)
-plt.show()
 
-# point = data[0][1][0]
-# fig = plt.figure()
-# ax = fig.add_subplot(221, projection='3d')
-# ax.scatter(point[0], point[1], point[2], s=2000, c='r')
-# ax.scatter(full_feasible[:,0], full_feasible[:,1], full_feasible[:,2], s=5, c=colors)
-# ax2 = fig.add_subplot(222, projection='3d')
-# ax2.scatter(point[3], point[4], point[5], s=2000, c='r')
-# ax2.scatter(full_feasible[:,3], full_feasible[:,4], full_feasible[:,5], s=5, c=colors)
-# ax3 = fig.add_subplot(223)
-# ax3.scatter(point[6], 0, s=200, c='r')
-# ax3.scatter(full_feasible[:,6], np.zeros(len(full_feasible)), s=10, c=colors)
-# plt.show()
+init_ent = entropy(means, taus, alphas, betas)
+info_gain_ent = [init_ent]
+cost_ent = [init_ent]
+info_gain_alpha0 = [init_ent]
+info_gain_max = [init_ent]
+cost_min = [init_ent]
+for i in range(80):
+    lam = np.hstack(((alphas - 1) / betas, means))
+    lam2 = np.hstack(((alphas2 - 1) / betas2, means2))
+    lam3 = np.hstack(((alphas3 - 1) / betas3, means3))
+    lam4 = np.hstack(((alphas4 - 1) / betas4, means4))
+    lam5 = np.hstack(((alphas5 - 1) / betas5, means5))
+
+    info_thetas, _ = max(data, key=lambda x: \
+    expected_info_gain(x[1], means, taus, alphas, betas, lam, ALPHA))
+    cost_thetas, _ = min(data, key=lambda x: \
+    expected_cost(x[1], lam2, ALPHA))
+    info_thetas2, _ = max(data, key=lambda x: \
+    expected_info_gain(x[1], means3, taus3, alphas3, betas3, lam3, 0))
+    max_infos, _ = max(data, key=lambda x: \
+    argmax_info_gain(x[1], means4, taus4, alphas4, betas4))
+    min_costs, _ = min(data, key=lambda x: \
+    argmin_cost(x[1], lam5))
+
+    means, taus, alphas, betas = update(means, taus, alphas, betas, info_thetas)
+    means2, taus2, alphas2, betas2 = update(means2, taus2, alphas2, betas2, cost_thetas)
+    means3, taus3, alphas3, betas3 = update(means3, taus3, alphas3, betas3, info_thetas2)
+    means4, taus4, alphas4, betas4 = update(means4, taus4, alphas4, betas4, max_infos)
+    means5, taus5, alphas5, betas5 = update(means5, taus5, alphas5, betas5, min_costs)
+
+    info_gain_ent.append(entropy(means, taus, alphas, betas))
+    cost_ent.append(entropy(means2, taus2, alphas2, betas2))
+    info_gain_alpha0.append(entropy(means3, taus3, alphas3, betas3))
+    info_gain_max.append(entropy(means4, taus4, alphas4, betas4))
+    cost_min.append(entropy(means5, taus5, alphas5, betas5))
+    print i
+ax.plot(info_gain_ent, label='max expected info')
+ax.plot(info_gain_alpha0, label='max expected info, alpha=0')
+ax.plot(info_gain_max, label='max info gain')
+ax.plot(cost_min, label='min cost')
+ax.plot(cost_ent, label='min expected cost')
+ax.set_xlabel('iteration')
+ax.set_ylabel('entropy')
+plt.legend(loc='upper right')
+plt.show()
