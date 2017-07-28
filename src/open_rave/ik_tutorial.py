113b12f1d7931f3671318276ba933dec9ada31c0
Ravi Pandya <rpandya922@berkeley.edu>
2017-07-27 17:02:10 -0700

    initial testing with man1 ik

---
+++ src/open_rave/ik_tutorial.py
@@ -0,0 +1,79 @@
+import time
+import openravepy
+if not __openravepy_build_doc__:
+    from openravepy import *
+    from numpy import *
+
+def main(env,options):
+    "Main example code."
+    env.Load('robots/man1.zae')
+    robot = env.GetRobots()[0]
+    newrobots = []
+    for ind in range(options.maxnumber):
+        newrobot = RaveCreateRobot(env,robot.GetXMLId())
+        newrobot.Clone(robot,0)
+        for link in newrobot.GetLinks():
+            for geom in link.GetGeometries():
+                geom.SetTransparency(options.transparency)
+        newrobots.append(newrobot)
+    for link in robot.GetLinks():
+        for geom in link.GetGeometries():
+            geom.SetTransparency(options.transparency)
+
+    ikmodel = databases.inversekinematics.InverseKinematicsModel(robot, iktype=IkParameterization.Type.Transform6D)
+    if not ikmodel.load():
+        ikmodel.autogenerate()
+    with env:
+        # move the robot in a random collision-free position and call the IK
+        while True:
+            lower,upper = [v[ikmodel.manip.GetArmIndices()] for v in ikmodel.robot.GetDOFLimits()]
+            robot.SetDOFValues(random.rand()*(upper-lower)+lower,ikmodel.manip.GetArmIndices()) # set random values
+            if not robot.CheckSelfCollision():
+                solutions = ikmodel.manip.FindIKSolutions(ikmodel.manip.GetTransform(),IkFilterOptions.CheckEnvCollisions)
+                if solutions is not None and len(solutions) > 0: # if found, then break
+                    break
+        print 'found %d solutions, rendering solutions:'%len(solutions)
+        if len(solutions) < options.maxnumber:
+            inds = range(len(solutions))
+        else:
+            inds = array(linspace(0,len(solutions)-1,options.maxnumber),int)
+        for i,ind in enumerate(inds):
+            print ind
+            newrobot = newrobots[i]
+            env.Add(newrobot,True)
+            newrobot.SetTransform(robot.GetTransform())
+            newrobot.SetDOFValues(solutions[ind],ikmodel.manip.GetArmIndices())
+    env.UpdatePublishedBodies()
+    print('waiting...')
+    time.sleep(20)
+    # remove the robots
+    for newrobot in newrobots:
+        env.Remove(newrobot)
+    del newrobots
+    while True:
+        continue
+
+from optparse import OptionParser
+from openravepy.misc import OpenRAVEGlobalArguments
+
+@openravepy.with_destroy
+def run(args=None):
+    """Command-line execution of the example.
+
+    :param args: arguments for script to parse, if not specified will use sys.argv
+    """
+    parser = OptionParser(description='Shows how to generate a 6D inverse kinematics solver and use it for getting all solutions.')
+    OpenRAVEGlobalArguments.addOptions(parser)
+    parser.add_option('--scene',action="store",type='string',dest='scene',default='data/pr2test1.env.xml',
+                      help='Scene file to load (default=%default)')
+    parser.add_option('--transparency',action="store",type='float',dest='transparency',default=0.8,
+                      help='Transparency for every robot (default=%default)')
+    parser.add_option('--maxnumber',action="store",type='int',dest='maxnumber',default=10,
+                      help='Max number of robots to render (default=%default)')
+    parser.add_option('--manipname',action="store",type='string',dest='manipname',default=None,
+                      help='name of manipulator to use (default=%default)')
+    (options, leftargs) = parser.parse_args(args=args)
+    OpenRAVEGlobalArguments.parseAndCreateThreadedUser(options,main,defaultviewer=True)
+
+if __name__ == "__main__":
+    run()
