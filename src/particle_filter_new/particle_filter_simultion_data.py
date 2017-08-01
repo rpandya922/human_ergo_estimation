91e3efff8b84b1933f5322fa7f558aba9415cd79
Ravi Pandya <rpandya922@berkeley.edu>
2017-08-01 11:56:56 -0700

    code cleanup

--- src/particle_filter_new/particle_filter_simultion_data.py
+++ src/particle_filter_new/particle_filter_simultion_data.py
@@ -216,9 +216,9 @@ def plot_updates(all_data):
         plt.pause(0.2)
     plt.show()
 def plot_feas(data):
-    fig, axes = plt.subplots(nrows=2, ncols=4)
+    fig, axes = plt.subplots(nrows=4, ncols=4)
     axes = np.ndarray.flatten(np.array(axes))
-    fig2, axes2 = plt.subplots(nrows=2, ncols=4)
+    fig2, axes2 = plt.subplots(nrows=4, ncols=4)
     axes2 = np.ndarray.flatten(np.array(axes2))
     for (i, (theta, feasible)) in enumerate(data):
         ax = axes[i]
@@ -240,6 +240,20 @@ def plot_feas(data):
     fig.suptitle("Feasible sets to choose from (dim 1&2)")
     fig2.suptitle("Feasible sets to choose from (dim 3&4)")
     plt.pause(0.2)
+def plot_belief(ax, particles, ground_truth):
+    data_means = particles.T
+    kernel = kde(data_means)
+    xx, yy = np.mgrid[-3:1.75:100j, -3:1.5:100j]
+    positions = np.vstack([xx.ravel(), yy.ravel()])
+    f = np.reshape(kernel(positions).T, xx.shape)
+    cfset = ax.contourf(xx, yy, f, cmap='Greens')
+    cset = ax.contour(xx, yy, f, colors='k')
+    ax.clabel(cset, inline=1, fontsize=10)
+    ax.scatter(ground_truth[0], ground_truth[1], c='C3', s=200, zorder=2)
+def plot_belief_update(ax, particles, theta, feasible, ground_truth):
+    plot_belief(ax, particles, ground_truth)
+    ax.scatter(feasible[:,0], feasible[:,1], c='C0')
+    ax.scatter(theta[0], theta[1], c='C2', s=200, zorder=2)
 #########################################################
 f = np.load('./sim_translation_training_data.npz')
 all_data = f['data']
@@ -263,97 +277,57 @@ particles += mins
 weights = np.ones(NUM_PARTICLES) / NUM_PARTICLES
 weights = np.array(weights) / np.sum(weights)
 dist = SetWeightsParticleDistribution(particles, weights, cost, w=TRUE_WEIGHTS, ALPHA_I=ALPHA_I, ALPHA_O=ALPHA_O)
+plot_feas(data)
 
-# plot_feas(data)
-# plt.show()
 def info_gain(dist, x):
     return (x, dist.info_gain(x[1], num_boxes=20), dist.expected_cost(x[1]))
-# if __name__ == '__main__':
-#     pool = mp.Pool(8)
-fig, axes = plt.subplots(nrows=4, ncols=3)
-axes = np.ndarray.flatten(np.array(axes))
-fig2, axes2 = plt.subplots(nrows=4, ncols=3)
-axes2 = np.ndarray.flatten(np.array(axes2))
-# bar_fig, bar_axes = plt.subplots(nrows=4, ncols=3)
-# bar_axes = np.ndarray.flatten(np.array(bar_axes))
-fig.suptitle('dim 1&2 Particles: ' + str(NUM_PARTICLES) + ' alpha_i: ' + str(ALPHA_I) +\
-             ' alpha_o: ' + str(ALPHA_O))
-fig2.suptitle('dim 3&4 Particles: ' + str(NUM_PARTICLES) + ' alpha_i: ' + str(ALPHA_I) +\
-             ' alpha_o: ' + str(ALPHA_O))
-ax = axes[0]
-ax2 = axes2[0]
-data_means = np.array(dist.particles)[:,:2].T
-kernel = kde(data_means)
-xx, yy = np.mgrid[-3:1.75:100j, -3:1.5:100j]
-positions = np.vstack([xx.ravel(), yy.ravel()])
-f = np.reshape(kernel(positions).T, xx.shape)
-cfset = ax.contourf(xx, yy, f, cmap='Greens')
-cset = ax.contour(xx, yy, f, colors='k')
-ax.clabel(cset, inline=1, fontsize=10)
-ax.scatter(TRUE_MEAN[0], TRUE_MEAN[1], c='C3', s=200, zorder=2)
+if __name__ == '__main__':
+    pool = mp.Pool(8)
+    fig, axes = plt.subplots(nrows=4, ncols=3)
+    axes = np.ndarray.flatten(np.array(axes))
+    fig2, axes2 = plt.subplots(nrows=4, ncols=3)
+    axes2 = np.ndarray.flatten(np.array(axes2))
+    bar_fig, bar_axes = plt.subplots(nrows=4, ncols=3)
+    bar_axes = np.ndarray.flatten(np.array(bar_axes))
+    fig.suptitle('dim 1&2 Particles: ' + str(NUM_PARTICLES) + ' alpha_i: ' + str(ALPHA_I) +\
+                 ' alpha_o: ' + str(ALPHA_O))
+    fig2.suptitle('dim 3&4 Particles: ' + str(NUM_PARTICLES) + ' alpha_i: ' + str(ALPHA_I) +\
+                 ' alpha_o: ' + str(ALPHA_O))
+    ax = axes[0]
+    ax2 = axes2[0]
 
-data_means = np.array(dist.particles)[:,2:4].T
-kernel = kde(data_means)
-xx, yy = np.mgrid[-2:1:100j, -3:0.5:100j]
-positions = np.vstack([xx.ravel(), yy.ravel()])
-f = np.reshape(kernel(positions).T, xx.shape)
-cfset = ax2.contourf(xx, yy, f, cmap='Greens')
-cset = ax2.contour(xx, yy, f, colors='k')
-ax2.clabel(cset, inline=1, fontsize=10)
-ax2.scatter(TRUE_MEAN[2], TRUE_MEAN[3], c='C3', s=200, zorder=2)
-plt.pause(0.2)
-for i in range(1, 12):
-    # func = partial(info_gain, dist)
-    # pooled = pool.map(func, data)
-    # # pooled = [func(x) for x in data]
-    # print
-    # expected_infos = [sample[1] for sample in pooled]
-    # expected_costs = [sample[2] for sample in pooled]
-    # # max_idx = np.argmax(expected_infos)
-    # max_idx = np.argmin(expected_costs)
-    # (theta, feasible) = pooled[max_idx][0]
-    # actual_infos = []
-    # ent_before = dist.entropy(num_boxes=20)
-    # for j in range(len(data)):
-    #     t, f = data[j]
-    #     d = SetWeightsParticleDistribution(dist.particles, dist.weights, dist.cost, dist.w, dist.ALPHA_I, dist.ALPHA_O)
-    #     d.weights = d.reweight(t, f)
-    #     actual_infos.append(ent_before - d.entropy(num_boxes=20))
-    (theta, feasible) = data[i]
-    dist.weights = dist.reweight_vectorized(theta, feasible)
-    dist.resample()
-
-    target.SetTransform(poses[i])
-
-    ax = axes[i]
-    ax2 = axes2[i]
-    # bar_ax = bar_axes[i]
-    data_means = np.array(dist.particles)[:,:2].T
-    kernel = kde(data_means)
-    xx, yy = np.mgrid[-3:1.75:100j, -3:1.5:100j]
-    positions = np.vstack([xx.ravel(), yy.ravel()])
-    f = np.reshape(kernel(positions).T, xx.shape)
-    cfset = ax.contourf(xx, yy, f, cmap='Greens')
-    cset = ax.contour(xx, yy, f, colors='k')
-    ax.clabel(cset, inline=1, fontsize=10)
-    ax.scatter(feasible[:,0], feasible[:,1], c='C0')
-    ax.scatter(theta[0], theta[1], c='C2', s=200, zorder=2)
-    ax.scatter(TRUE_MEAN[0], TRUE_MEAN[1], c='C3', s=200, zorder=2)
-
-    data_means = np.array(dist.particles)[:,2:4].T
-    kernel = kde(data_means)
-    xx, yy = np.mgrid[-2:1:100j, -2.5:0.5:100j]
-    positions = np.vstack([xx.ravel(), yy.ravel()])
-    f = np.reshape(kernel(positions).T, xx.shape)
-    cfset = ax2.contourf(xx, yy, f, cmap='Greens')
-    cset = ax2.contour(xx, yy, f, colors='k')
-    ax2.clabel(cset, inline=1, fontsize=10)
-    ax2.scatter(feasible[:,2], feasible[:,3], c='C0')
-    ax2.scatter(theta[2], theta[3], c='C2', s=200, zorder=2)
-    ax2.scatter(TRUE_MEAN[2], TRUE_MEAN[3], c='C3', s=200, zorder=2)
-
-    # bar_ax.bar(np.arange(len(data)), expected_infos, 0.35, color='C0', label='expected info gain')
-    # bar_ax.bar(np.arange(len(data)) + 0.35, actual_infos, 0.35, color='C1', label='actual info gain')
-    # bar_ax.bar(max_idx, expected_infos[max_idx], 0.35, color='C2', label='chosen set expected info')
+    plot_belief(ax, np.array(dist.particles)[:,:2], TRUE_MEAN[:2])
+    plot_belief(ax2, np.array(dist.particles)[:,2:], TRUE_MEAN[2:])
     plt.pause(0.2)
-plt.show()
+
+    for i in range(1, 12):
+        func = partial(info_gain, dist)
+        pooled = pool.map(func, data)
+        print
+        expected_infos = [sample[1] for sample in pooled]
+        expected_costs = [sample[2] for sample in pooled]
+        max_idx = np.argmax(expected_infos)
+        # max_idx = np.argmin(expected_costs)
+        (theta, feasible) = pooled[max_idx][0]
+        actual_infos = []
+        ent_before = dist.entropy(num_boxes=20)
+        for j in range(len(data)):
+            t, f = data[j]
+            d = SetWeightsParticleDistribution(dist.particles, dist.weights, dist.cost, dist.w, dist.ALPHA_I, dist.ALPHA_O)
+            d.weights = d.reweight(t, f)
+            actual_infos.append(ent_before - d.entropy(num_boxes=20))
+        # (theta, feasible) = data[i]
+        dist.weights = dist.reweight_vectorized(theta, feasible)
+        dist.resample()
+
+        target.SetTransform(poses[i])
+        ax = axes[i]
+        ax2 = axes2[i]
+        bar_ax = bar_axes[i]
+        plot_belief_update(ax, np.array(dist.particles)[:,:2], theta, feasible, TRUE_MEAN)
+        plot_belief_update(ax2, np.array(dist.particles)[:,2:], theta[2:], feasible[:,2:], TRUE_MEAN[2:])
+        bar_ax.bar(np.arange(len(data)), expected_infos, 0.35, color='C0', label='expected info gain')
+        bar_ax.bar(np.arange(len(data)) + 0.35, actual_infos, 0.35, color='C1', label='actual info gain')
+        bar_ax.bar(max_idx, expected_infos[max_idx], 0.35, color='C2', label='chosen set expected info')
+        plt.pause(0.2)
+    plt.show()
